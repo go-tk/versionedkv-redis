0a426f846dbfb300e65b986bd986d9835d0b9d76
Roy O'Young <roy2220@outlook.com>
2021-01-18 23:24:09 +0800

    Add test

--- redisstorage/redisstorage_test.go
+++ redisstorage/redisstorage_test.go
@@ -1,18 +1,33 @@
 package redisstorage_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/alicebob/miniredis/v2"
 	"github.com/go-redis/redis/v8"
 	"github.com/go-tk/versionedkv"
 	. "github.com/go-tk/versionedkv-redis/redisstorage"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRedisStorage(t *testing.T) {
 	versionedkv.DoTestStorage(t, func() (versionedkv.Storage, error) {
 		return makeStorage()
 	})
+}
+
+func TestRedisStorage_Close(t *testing.T) {
+	s, err := makeStorage()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	time.AfterFunc(1*time.Second, func() {
+		s.Close() // WaitForValue should fail with error ErrStorageClosed
+	})
+	_, _, err = s.WaitForValue(context.Background(), "foo", nil)
+	assert.Equal(t, err, versionedkv.ErrStorageClosed)
 }
 
 type storage struct {
