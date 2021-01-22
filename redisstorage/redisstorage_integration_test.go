20db73b92ad008a7a67011c21387a478956b9a7f
Roy O'Young <roy2220@outlook.com>
2021-01-22 22:42:32 +0800

    Improve integration test

--- redisstorage/redisstorage_integration_test.go
+++ redisstorage/redisstorage_integration_test.go
@@ -3,12 +3,15 @@
 package redisstorage_test
 
 import (
+	"context"
 	"os"
 	"strconv"
 	"strings"
 	"sync/atomic"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/go-tk/backoff"
 	"github.com/go-tk/versionedkv"
 	. "github.com/go-tk/versionedkv-redis/redisstorage"
 )
@@ -25,6 +28,33 @@ func makeStorage() (storage, error) {
 		as = append(as, rn+":6379")
 	}
 	c := redis.NewClusterClient(&redis.ClusterOptions{Addrs: as})
+	b := backoff.New(backoff.Options{
+		MinDelay:            100 * time.Millisecond,
+		MaxDelay:            3 * time.Second,
+		MaxNumberOfAttempts: 10,
+	})
+	for {
+		css, err := c.ClusterSlots(context.Background()).Result()
+		if err != nil {
+			if err2 := b.Do(); err2 != nil {
+				c.Close()
+				return storage{}, err
+			}
+			continue
+		}
+		var n int
+		for _, cs := range css {
+			n += len(cs.Nodes)
+		}
+		if n < len(as) {
+			if err := b.Do(); err != nil {
+				c.Close()
+				return storage{}, err
+			}
+			continue
+		}
+		break
+	}
 	sid := int(atomic.AddInt32(&lastStorageID, 1))
 	s := New(c, Options{
 		Prefix: "versionedkv" + strconv.Itoa(sid),
